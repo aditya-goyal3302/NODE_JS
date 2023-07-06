5823f2e5a3ffb0ce411753f869aa04b7d8e2a9f3
aditya-goyal3302 <agoyal2505.ca21@chitkara.edu.in>
2023-07-06 22:01:30 +0530

    restart today

--- shoping_app/app.js
+++ shoping_app/app.js
@@ -6,6 +6,11 @@ app.set('title','My site')
 const rootDir = require("./util/path");
 const admin_route = require("./routes/admin") ;
 const shop_route = require("./routes/shop") ;
+// const expresshbs = require('express-handlebars');
+
+// app.engine('hbs',expresshbs());
+app.set('view engine','pug');
+app.set('views','views')
 
 app.use(express.static(path.join(__dirname,'public')));
 
@@ -15,7 +20,7 @@ app.use('/admin',admin_route.routes);
 app.use(shop_route);
 
 app.use((req,res,next)=>{
-    res.status(404).sendFile(path.join(rootDir,'views','404.html'));
+    res.status(404).render('404',{pageTitle:'404'});
 });
 
 app.listen(3000);
--- shoping_app/routes/shop.js
+++ shoping_app/routes/shop.js
@@ -7,8 +7,11 @@ const rootDir = require('../util/path');
 const router = express.Router();
 const admin = require('./admin');
 router.get('/', (req, res, next) => {
-    console.log(admin.products);
-  res.sendFile(path.join(rootDir, 'views', 'shop.html'));
+    // console.log(admin.products);
+    //res.sendFile(path.join(rootDir, 'views', 'shop.pug'));
+    const pro = admin.products;
+    res.render('shop',{prods:pro, pageTitle: "Shop",path:'/shop'});
+
 });
 
 module.exports = router;
