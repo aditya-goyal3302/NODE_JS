c6ea078fbf7366f8cbed77f3c12ea48e5acc5582
aditya-goyal3302 <agoyal2505.ca21@chitkara.edu.in>
2023-07-07 09:29:24 +0530

    package update

--- shoping_app/app.js
+++ shoping_app/app.js
@@ -6,10 +6,10 @@ app.set('title','My site')
 const rootDir = require("./util/path");
 const admin_route = require("./routes/admin") ;
 const shop_route = require("./routes/shop") ;
-// const expresshbs = require('express-handlebars');
+const expressHbs = require('express-handlebars');
 
-// app.engine('hbs',expresshbs());
-app.set('view engine','pug');
+app.engine('handlebars',expressHbs());
+app.set('view engine','handlebars');
 app.set('views','views')
 
 app.use(express.static(path.join(__dirname,'public')));
