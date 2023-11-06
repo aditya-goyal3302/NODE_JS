98727c1d5b3f11ef6843c2e58f5853d4937f0597
aditya-goyal3302 <agoyal2505.ca21@chitkara.edu.in>
2023-11-06 14:12:05 +0530

    till today update

--- shoping_app/app.js
+++ shoping_app/app.js
@@ -1,26 +1,24 @@
-const express = require('express');
 const path = require('path');
-const bodyparser = require("body-parser")
+
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const errorController = require('./controllers/error');
+
 const app = express();
-app.set('title','My site')
-const rootDir = require("./util/path");
-const admin_route = require("./routes/admin") ;
-const shop_route = require("./routes/shop") ;
-const expressHbs = require('express-handlebars');
 
-app.engine('handlebars',expressHbs());
-app.set('view engine','handlebars');
-app.set('views','views')
+app.set('view engine', 'ejs');
+app.set('views', 'views');
 
-app.use(express.static(path.join(__dirname,'public')));
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
-app.use(bodyparser.urlencoded({extended:false}));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/admin',admin_route.routes);
-app.use(shop_route);
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
 
-app.use((req,res,next)=>{
-    res.status(404).render('404',{pageTitle:'404'});
-});
+app.use(errorController.get404);
 
 app.listen(3000);
--- shoping_app/routes/shop.js
+++ shoping_app/routes/shop.js
@@ -2,16 +2,18 @@ const path = require('path');
 
 const express = require('express');
 
-const rootDir = require('../util/path');
+const shopController = require('../controllers/shop');
 
 const router = express.Router();
-const admin = require('./admin');
-router.get('/', (req, res, next) => {
-    // console.log(admin.products);
-    //res.sendFile(path.join(rootDir, 'views', 'shop.pug'));
-    const pro = admin.products;
-    res.render('shop',{prods:pro, pageTitle: "Shop",path:'/shop'});
 
-});
+router.get('/', shopController.getIndex);
+
+router.get('/products', shopController.getProducts);
+
+router.get('/cart', shopController.getCart);
+
+router.get('/orders', shopController.getOrders);
+
+router.get('/checkout', shopController.getCheckout);
 
 module.exports = router;
