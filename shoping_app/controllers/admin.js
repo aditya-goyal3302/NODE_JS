db231f9bb787fed84bc16029867a2df05ee64938
aditya-goyal3302 <agoyal2505.ca21@chitkara.edu.in>
2024-02-01 10:23:44 +0530

    trf

--- shoping_app/controllers/admin.js
+++ shoping_app/controllers/admin.js
@@ -156,7 +156,7 @@ exports.postEditProduct = (req, res, next) => {
 exports.getProducts = (req, res, next) => {
   Product.find({userId:req.user._id})
     .then(products => {
-      console.log(products);
+      // console.log(products);
       res.render('admin/products', {
         prods: products,
         pageTitle: 'Admin Products',
